551cbbd6458965fa1a34b3cd7285c2de765f7640
SaravanaSai <saravanasai002@gmail.com>
2025-03-30 22:43:41 +0530

    WIP basic docker & app reload for dev added

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ import {
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const host = process.env.HOST || '0.0.0.0';
+  const port = process.env.PORT || 8000;
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(port, host);
+  console.log(`🚀 Server is running at http://${host}:${port}`);
 }
 bootstrap();
